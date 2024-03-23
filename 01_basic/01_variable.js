3d0d677712adf817f25fa39d5cb5ffbaca8ef47c
Anit Raj Shahi <70832267+AnitShahi@users.noreply.github.com>
2024-03-23 09:29:39 +0000

    variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "anit@google.com"
+var accountPassword = "12345"
+accountCity = "ktm"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "anit.gmail.com"
+accountPassword = "21212121"
+accountCity = "npj"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
